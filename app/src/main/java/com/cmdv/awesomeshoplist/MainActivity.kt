0b2fcdbb17a289ad73429f2451442656d41f151d
Matías <matias.delv.dom@gmail.com>
2020-04-11 19:37:20 -0300

    Added core modules:
    core
    domain
    data
    components

--- app/src/main/java/com/cmdv/awesomeshoplist/MainActivity.kt
+++ app/src/main/java/com/cmdv/awesomeshoplist/MainActivity.kt
@@ -2,17 +2,14 @@ package com.cmdv.awesomeshoplist
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,7 +28,6 @@ class MainActivity : AppCompatActivity() {
 		}
 		val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 		val navView: NavigationView = findViewById(R.id.nav_view)
-		val navController = findNavController(R.id.nav_host_fragment)
 		// Passing each menu ID as a set of Ids because each
 		// menu should be considered as top level destinations.
 		appBarConfiguration = AppBarConfiguration(
@@ -39,8 +35,12 @@ class MainActivity : AppCompatActivity() {
 				R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
 			), drawerLayout
 		)
-		setupActionBarWithNavController(navController, appBarConfiguration)
-		navView.setupWithNavController(navController)
+
+		val drawerToggle = ActionBarDrawerToggle(
+			this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+		)
+		drawerToggle.syncState()
+		drawerLayout.addDrawerListener(drawerToggle)
 	}
 
 	override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -49,8 +49,4 @@ class MainActivity : AppCompatActivity() {
 		return true
 	}
 
-	override fun onSupportNavigateUp(): Boolean {
-		val navController = findNavController(R.id.nav_host_fragment)
-		return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-	}
 }
